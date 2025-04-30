b2e58259d433c525bece6eb722da6cd32e3868f6
Emmanuel Njungé Ngugi <ngugiemmanuelnjunge@gmail.com>
2025-04-30 08:16:39 +0300

    Implemented controlled components for React lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,23 +5,33 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState("");
+  const [itemList, setItemList] = useState(items);
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(newSearch) {
+    setSearchText(newSearch);
+  }
 
-    return item.category === selectedCategory;
-  });
+  function handleItemFormSubmit(newItem) {
+    setItemList([...itemList, newItem]);
+  }
+
+  const filteredItems = itemList.filter(
+    (item) =>
+      item.name.toLowerCase().includes(searchText.toLowerCase()) &&
+      (selectedCategory === "All" || item.category === selectedCategory)
+  );
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
+      <Filter search={searchText} onSearchChange={handleSearchChange} onCategoryChange={handleCategoryChange} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
